90505288bb5b993f62288b527b4a84d6a7d8d7da
Gecko525 <577715007@qq.com>
2019-12-28 16:55:26 +0800

    Create vue_deepCopy.js

---
+++ js/vue_deepCopy.js
@@ -0,0 +1,28 @@
+function deepCopy(obj, cache=[]) {
+    // 如果是null或者不是object直接返回
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+   
+    // 如果传入的对象与缓存的相等, 则递归结束, 这样防止循环
+    /**
+     * 类似下面这种
+     * var a = {b:1}
+     * a.c = a
+     * 资料: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value
+     */
+    const hit = cache.filter(c => c.orignal === obj)[0];
+    if (hit) {
+        return hit.copy;
+    }
+    const copy = Array.isArray(obj) ? [] : {};
+    // 将copy首先放入cache, 因为我们需要在递归deepCopy的时候引用它
+    cache.push({
+        orignal: obj,
+        copy
+    });
+    Object.keys(obj).forEach(key => {
+        copy[key] = deepCopy(obj[key], cache);
+    })
+    return copy;
+}
